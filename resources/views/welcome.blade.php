e210ada340164c34067089eff5ccc10d1ffaff78
armando barra p <armando.barra.p@gmail.com>
2025-01-25 22:31:55 -0300

    Added bootstrap login and details in colo in navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <link rel="icon" href="https://sinstress.cl/wp-content/uploads/2023/09/ico-sinstress-1.ico" type="image/x-icon">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
