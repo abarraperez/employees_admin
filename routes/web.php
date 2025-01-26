08931a1dad1e206bcbe0c7ffe847387760d84645
armando barra p <armando.barra.p@gmail.com>
2025-01-26 19:48:34 -0300

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,10 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/agenda', \App\Http\Controllers\AgendaController::class)->name('agenda'); 
+
+Route::get('/events', [\App\Http\Controllers\AppointmentController::class, "index"])->name('events.index'); 
+Route::post('/events', [\App\Http\Controllers\AppointmentController::class, "store"])->name('events.store');
+Route::get('/events/{appointment}', [\App\Http\Controllers\AppointmentController::class, "show"])->name('events.show');
+Route::get('/events/{appointment}/edit', [\App\Http\Controllers\AppointmentController::class, "edit"])->name('events.edit');
+Route::put('/events/{appointment}', [\App\Http\Controllers\AppointmentController::class, "update"])->name('events.update');
+Route::delete('/events/{appointment}', [\App\Http\Controllers\AppointmentController::class, "destroy"])->name('events.destroy');
